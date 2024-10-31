86e8cd32166c02076954c265c93fad7535ac3779
kcrystal120815 <kcrystal120815@gmail.com>
2024-10-31 17:48:03 +0900

    콜백함수 매개변수

--- a-beginning/13-double-each-number-in-array.js
+++ a-beginning/13-double-each-number-in-array.js
@@ -11,7 +11,7 @@ function solution(numbers) {
 - 배열의 각 요소에 대해 하나씩 차례대로 콜백 함수를 자동으로 호출 및 실행하고, 실행될 때마다 새로운 요소값을 currentValue에 할당하여, 새로운 배열을 반환하는 배열 메서드
 - Array인 객체에만 map을 직접 사용 가능 (객체/문자열 X)
 - 구조: array.map(callback, thisArg);
-   callback(콜백 함수): 배열의 각 요소에 대해 실행할 함수. 아래 3개의 매개변수가 callback함수로 전달될 수 있음(선택사항)
+   callback(콜백 함수): 배열의 각 요소에 대해 실행할 함수. 아래 3개의 매개변수가 callback함수로 전달될 수 있음(선택사항. 필요한 경우 (callback(currntValue,index,array))로 작성)
    - currentValue(현재값): 현재 처리 중인 요소
    - index: 배열 내에서 현재 요소의 순서
    - array: map 메서드를 호출한 배열의 이름
